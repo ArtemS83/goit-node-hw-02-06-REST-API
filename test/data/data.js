ce6c9c0e7765831cf179e0cba6f97e62f899d547
Artem Sergieiev <66088984+ArtemS83@users.noreply.github.com>
2021-06-07 00:11:43 +0200

    unit tests

---
+++ test/data/data.js
@@ -0,0 +1,20 @@
+const newContact = {
+  name: 'XXXXX',
+  email: 'xxxxx@x.com',
+  phone: '0000-000-000',
+};
+
+const newUser = {
+  name: 'newUser',
+  email: 'newUser@gmail.com',
+  password: '1234567',
+};
+
+const newUser2 = {
+  name: 'newUser2',
+  email: 'newUser2@gmail.com',
+  subscription: 'pro',
+  password: '1234567',
+};
+
+module.exports = { newContact, newUser, newUser2 };
